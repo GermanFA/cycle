2f97fc3ad5faac3fb9f5591719c10b58de2bfdc6
GermanFA <79627686+GermanFA@users.noreply.github.com>
2021-02-27 07:01:32 +0300

    Добавьте файлы проекта.

---
+++ cycle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTray;
+            string message;
+
+            Console.Write("Введите количество попыток:");
+            countTray = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сообщение:");
+            message = Console.ReadLine();
+
+
+            for (int i = 0; i < countTray; i++) 
+            {
+                Console.WriteLine("Ваша " + i + " попытка. Ваше сообщение:" + message);
+
+            }
+           
+        }
+    }
+}
